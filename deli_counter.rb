ae3bc8312ebd14e3957b1f6545681c99f30f5829
Jon Mackey <jon@jonmackey.party>
2018-11-13 20:23:08 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -3,7 +3,7 @@ def line(katz_deli)
     puts "The line is currently empty."
   elsif katz_deli.length > 0 
     counter = 0
-    line = katz_deli.map{|x|"#{counter+=1}.  #{x}"}.join
+    line = katz_deli.map{|x|"#{counter+=1}. #{x}"}.join
     puts "The line is currently: #{line}"
   end 
 end
